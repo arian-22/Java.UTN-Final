04812e0fd417a2033a94a7b3d5e82906ef7a7bfc
arian <arian@Arian-Ubuntu>
2017-08-10 12:17:22 -0300

    v

--- src/entidades/Vehiculos.java
+++ src/entidades/Vehiculos.java
@@ -1,5 +1,83 @@
 package entidades;
 
 public class Vehiculos {
-
+	private String patente, marca,modelo,transmision,estado,baul,tipo,foto;
+	private int cantAsientos,anio,precio,km;
+	
+	
+	public int getKm() {
+		return km;
+	}
+	public void setKm(int km) {
+		this.km = km;
+	}
+	public String getPatente() {
+		return patente;
+	}
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getTransmision() {
+		return transmision;
+	}
+	public void setTransmision(String transmision) {
+		this.transmision = transmision;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getBaul() {
+		return baul;
+	}
+	public void setBaul(String baul) {
+		this.baul = baul;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public int getCantAsientos() {
+		return cantAsientos;
+	}
+	public void setCantAsientos(int cantAsientos) {
+		this.cantAsientos = cantAsientos;
+	}
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+ 
+	
+	
 }
